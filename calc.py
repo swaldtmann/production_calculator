f89272447a49d5d815d51e83068295f95d3da14d
Woo <stephan@waldtmann.de>
2023-07-13 13:59:58 +0200

    Calculating full set of bridges and matrixes

--- calc.py
+++ calc.py
@@ -6,6 +6,7 @@
 import json
 from collections import defaultdict
 import sys
+import math
 
 with open('products.json', 'r') as f:
     recipes = json.load(f)
@@ -22,4 +23,23 @@ def count_raw_ingredients(recipe):
 
     return dict(ingredients)
 
-print(count_raw_ingredients('quadruple_axe'), "Batch Size: "+ str(recipes["quadruple_axe"]["batch_size"]))
+#for product in ["Large Optronic Bridge", "Large Optronic Matrix"]: #  56 / 20
+#    print(count_raw_ingredients(product), "Batch Size: "+ str(recipes[product]["batch_size"]))
+
+total_ingredients = defaultdict(int)
+print("Full set of CPU Extenders needs:")
+bridge_name = "Large Optronic Bridge"
+brigde_count = 56
+print(f"{brigde_count} x {bridge_name}:")
+for ing, am in sorted(count_raw_ingredients(bridge_name).items()):
+    total_ingredients[ing] += am*brigde_count
+    print(f"{math.ceil(total_ingredients[ing]):6.0f} x {ing}")
+matrix_name = "Large Optronic Matrix"
+matrix_count = 20
+print(f"{matrix_count} x {matrix_name}:")
+for ing1, am1 in sorted(count_raw_ingredients(matrix_name).items()):
+    total_ingredients[ing1] += am1*matrix_count
+    print(f"{math.ceil(am1*matrix_count):6.0f} x {ing1}")
+print("Total:")
+for ing2, am2 in sorted(total_ingredients.items()):
+    print(f"{math.ceil(total_ingredients[ing2]):6.0f} x {ing2}")
