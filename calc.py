2faa1b22b1a9d5c181376a6d88d99e8b6014c684
Woo <stephan@waldtmann.de>
2023-07-13 10:43:55 +0200

    Added "batch_size"

--- calc.py
+++ calc.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# See https://stackoverflow.com/questions/60218284/find-the-count-of-raw-ingredients-using-recursion-in-python for basic idea!
+
 import json
 from collections import defaultdict
 import sys
 
-with open('recipes.json', 'r') as f:
+with open('products.json', 'r') as f:
     recipes = json.load(f)
-
-raw_ingredients = recipes["raw_ingredients"]
-del recipes["raw_ingredients"]
-
-#print(recipes)
-#print(raw_ingredients)
-
-#sys.exit()
 
 def count_raw_ingredients(recipe):
     ingredients = defaultdict(int)
-
-    for ingredient, amount in recipes[recipe].items():
-        if ingredient == "batch_size":
-            continue
-        if ingredient in raw_ingredients:
+    for ingredient, amount in recipes[recipe]["ingredients"].items():
+        if ingredient not in recipes:
             ingredients[ingredient] += amount
         else:
+            bs = recipes[ingredient]["batch_size"]
             for subingredient, subamount in count_raw_ingredients(ingredient).items():
-                ingredients[subingredient] += amount * subamount
+                ingredients[subingredient] += amount * subamount / bs
 
     return dict(ingredients)
 
